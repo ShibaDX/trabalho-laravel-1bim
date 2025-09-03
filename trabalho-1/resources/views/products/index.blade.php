c1fee9b497021b5320511ce72efbbc85f259eaaf
ShibaDX <kaikymassaki2005@gmail.com>
2025-09-03 20:30:09 -0300

    aula 03-09

---
+++ trabalho-1/resources/views/products/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Produtos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+        <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SohBug</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>Lista de Produtos</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Categoria</th>
+                    <th>Quantidade</th>
+                    <th>Preço</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>  
+            <tbody>
+                @foreach($products as $product)
+                    <tr>
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->nome }}</td>
+                        <td>{{ $product->categoria }}</td>
+                        <td>{{ $product->quantidade }}</td>
+                        <td>R${{ $product->preco }}</td>
+                        <td>
+                            <a href="{{ route('products.show', $product) }}">
+                                Detalhes
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>  
+        </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
